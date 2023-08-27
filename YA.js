4d9731005b6f3f7b15cf87ffcf29659c36a2c279
AvitalGilburg <143225704+AvitalGilburg@users.noreply.github.com>
2023-08-27 13:49:33 -0400

    Delete YA.js

--- YA.js
+++
@@ -1,8 +0,0 @@
-const yamenu = document.querySelector(".yamenu");
-const navMenu = document.querySelector(".nav-menu");
-
-yamenu.addEventListener("click", () => {yamenu.classList.toggle("active"); navMenu.classList.toggle("active");})
-document.querySelector(".nav-link").forEach(n => n.addEventListener("click", () => {yamenu.classList.remove("active");navMenu.classList.remove("active");}))
-
-if yamenu:
-	window.open(
